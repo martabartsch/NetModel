7d203f4195686510faf37a9cceae4f6ad4b9d2b8
martabartsch <martabartsch@outlook.com>
2024-11-08 15:40:26 +0100

    Added BRV graph

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 
 using namespace std;
 
+vector<int> lastConnectedToRootPoints;
+
 
 vector<vector<int>> createEmptyMatrix(int size) {
     return vector<vector<int>>(size, vector<int>(size, 0));
+}
+
+void addBothSidesEdgeToMatrix(vector<vector<int>>& graph, int x, int y) {
+    graph[x][y] = 1;
+    graph[y][x] = 1;
 }
 
 vector<vector<int>> generateBRVGraph(int iterations) {
@@ -14,20 +21,20 @@ vector<vector<int>> generateBRVGraph(int iterations) {
     }
     else if(iterations == 1){
         vector<vector<int>> baseGraph = createEmptyMatrix(3);
-        baseGraph[0][1] = 1;
-        baseGraph[0][2] = 1;
-        baseGraph[1][0] = 1;
-        baseGraph[2][0] = 1;
+        addBothSidesEdgeToMatrix(baseGraph, 0, 1);
+        addBothSidesEdgeToMatrix(baseGraph, 0, 2);
+        lastConnectedToRootPoints = {1, 2};
         return baseGraph;
     }
-    else{
+    else{ // n = 2
+        vector <int> newConnectedToRootPoints;
         int prevSize = pow(3, iterations - 1);
         int newSize = prevSize * 3;
 
-        vector<vector<int>> prevGraph = generateBRVGraph(iterations - 1);
-        vector<vector<int>> newGraph = createEmptyMatrix(newSize);
+        vector<vector<int>> prevGraph = generateBRVGraph(iterations - 1); // 9x9 {4, 5, 7, 8 }
+        vector<vector<int>> newGraph = createEmptyMatrix(newSize); // 27x27
 
-        for (int i = 0; i < prevSize; i++) {
+        for (int i = 0; i < prevSize; i++) { // kopiowanie grafu
             for (int j = 0; j < prevSize; j++) {
                 // Pierwsza kopia w lewym górnym rogu
                 newGraph[i][j] = prevGraph[i][j];
@@ -39,16 +46,24 @@ vector<vector<int>> generateBRVGraph(int iterations) {
                 newGraph[i + 2 * prevSize][j + 2 * prevSize] = prevGraph[i][j];
             }
         }
-        cout<<prevSize<<endl;
-        for(int i = 0; i < prevSize; i++)
+
+        int leftSideDistanceFromRoot = prevSize;
+        for (int i = 0; i < lastConnectedToRootPoints.size(); i++)
         {
-            if(prevGraph[0][i]==1){
-                newGraph[0][prevSize+i] = 1;
-                newGraph[prevSize+i][0] = 1;
-                newGraph[0][2*prevSize+i] = 1;
-                newGraph[2*prevSize+i][0] = 1;
-            }
+            int newPointIndex = lastConnectedToRootPoints[i] + leftSideDistanceFromRoot;
+            addBothSidesEdgeToMatrix(newGraph, 0, newPointIndex);
+            newConnectedToRootPoints.push_back(newPointIndex);
         }
+
+        int rightSideDistanceFromRoot = prevSize * 2;
+        for (int i = 0; i < lastConnectedToRootPoints.size(); i++)
+        {
+            int newPointIndex = lastConnectedToRootPoints[i] + rightSideDistanceFromRoot;
+            addBothSidesEdgeToMatrix(newGraph, 0, newPointIndex);
+            newConnectedToRootPoints.push_back(newPointIndex);
+        }
+
+        lastConnectedToRootPoints = newConnectedToRootPoints;
         return newGraph;
     }
 }
@@ -67,7 +82,7 @@ int calculateTotalDistance(const vector<vector<int>>& graph) {
     int n = graph.size();
 
     for (int start = 0; start < n; start++) {
-        vector<int> distances(n, INT_MAX); // Ustal maksymalne odległości
+        vector<int> distances(n, INT_MAX); // Maksymalne odległości
         distances[start] = 0; // Odległość od startowego węzła do siebie samego to 0
         queue<int> q;
         q.push(start);
@@ -103,8 +118,8 @@ int main() {
         switch(num_model){
             case 0:
                 vector<vector<int>> graph = generateBRVGraph(value);
-                cout << "Macierz sąsiedztwa:" << endl;
-                displayMatrix(graph);
+//                cout << "Macierz sąsiedztwa:" << endl;
+                //displayMatrix(graph);
                 value = calculateTotalDistance(graph);
                 cout << value<< endl;
                 break;

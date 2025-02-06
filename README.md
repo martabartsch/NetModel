0e16261e57e05f3ff5f7b9fd57a531c7e8abd924
martabartsch <martabartsch@outlook.com>
2025-02-06 10:35:21 +0100

    Updated README.md and moved algorithms to a directory

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # NetModel
+
+NetModel implements several algorithms for constructing graphs in either adjacency list or adjacency matrix form and calculates the sum of all distances between pairs of vertices.
+
+### Implemented Models
+
+The program supports the following network models:
+
+| **Index** | **Model Name** | **Parameters**                           | **Description** |
+|-----------|--------------|------------------------------------------|----------------|
+| 0 | **Barabási–Ravasz–Vicsek (BRV)** | number of iterations                     | Generates a hierarchical scale-free network. |
+| 1 | **Lu–Su–Guo** | number of vertices                       | Constructs a deterministic network with small-world properties. |
+| 2 | **Iterative Growth** | number of iterations, `r`                | Expands a network by iteratively adding new vertices to existing edges. |
+| 3 | **DCN (Deterministic Complex Network)** | number of vertices                       | Creates a deterministic hierarchical network. |
+| 4 | **Kronecker Graph** | exponent, adjacency matrix (as a string) | Uses the Kronecker product to generate a self-similar network. |
+
+Each model allows the user to choose between two representations:
+
+1. adjacency list
+2. adjacency matrix (except for Iterative Growth)
+
+### Init in Ubuntu
+
+1. Clone the repository
+```bash
+    git clone https://github.com/martabartsch/NetModel.git
+    cd NetModel
+```
+
+2. Compilation and execution
+```bash
+    mkdir -p build
+    cd build
+    cmake ..
+    cmake --build .
+```
+3. Run the project
+```bash
+    ./NetModel
+```
+
+### Usage
+
+**Input Format**
+1. Enter the **model index** followed by the required **parameters** (refer to the table above for details).
+2. Choose the **graph representation**:
+    - **1:** Adjacency List
+    - **2:** Adjacency Matrix (not available for Iterative Growth)
+
+**Output Format**
+- The program outputs the **sum of all distances** in the generated graph and graph representation.
+
+#### Detailed Example Breakdown
+
+1. BRV Model
+```
+    0 4
+    11664
+```
+
+2. Lu–Su–Guo Model
+```
+    1 348
+    177472
+```
+
+3. Iterative Growth Model
+```bash
+    3 2 1
+    13
+```
+
+4. DCN Model
+```
+    4 91
+    7764
+```
+5. Kronecker
+```
+   6 4 0111110101110011000111110
+   293072
+```
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include<string>
-#include "BRV.h"
-#include "DCN.h"
-#include "IterativeGrowth.h"
-#include "LuSuGuo.h"
-#include "Kronecker.h"
-#include "Graph.h"
+#include "Models/BRV.h"
+#include "Models/DCN.h"
+#include "Models/IterativeGrowth.h"
+#include "Models/LuSuGuo.h"
+#include "Models/Kronecker.h"
+#include "Models/Graph.h"
 
 using namespace std;
 
@@ -14,44 +14,90 @@ int main() {
     int num_model, value;
     while(cin>>num_model>>value)
     {
+        int choice;
+        cout << "Choose the representation: "<<endl;
+        if(num_model == 3){
+            cout<<"1: Adjacency List"<<endl;
+        }
+        else{
+            cout << "1: Adjacency List, 2: Adjacency Matrix: "<<endl;
+        }
+        cin >> choice;
+        cout<<"-------"<<endl;
+        while (true) {
+            cin >> choice;
+            if ((num_model == 2 && choice == 1) || (num_model != 2 && (choice == 1 || choice == 2))) {
+                break;
+            }
+            cout << "Invalid choice. Please enter a valid option." << endl;
+        }
+
+        cout << "-------" << endl;
         switch(num_model) {
             case 0: {
                 BRV brv;
-                vector<vector<int>> adjList = brv.generateBRVAdjacencyList(value);
-                value = Graph::calculateTotalDistance(adjList);
+                if (choice == 1) {
+                    vector<vector<int>> adjList = brv.generateBRVAdjacencyList(value);
+                    value = Graph::calculateTotalDistance(adjList);
+                }
+                else if (choice == 2){
+                    vector<vector<int>> adjMatrix = brv.generateBRVAdjacencyMatrix(value);
+                    value = Graph::calculateTotalDistanceMatrix(adjMatrix);
+                }
                 cout << value << endl;
                 break;
             }
             case 1:{
                 LuSuGuo luSuGuo;
-                vector<vector<int>> adjList = luSuGuo.adjacencyListLuSuGuo(value);
-                value = Graph::calculateTotalDistance(adjList);
+                if (choice == 1) {
+                    vector<vector<int>> adjList = luSuGuo.adjacencyListLuSuGuo(value);
+                    value = Graph::calculateTotalDistance(adjList);
+                }
+                else if (choice == 2){
+                    vector<vector<int>> adjMatrix = luSuGuo.adjacencyMatrixLuSuGuo(value);
+                    value = Graph::calculateTotalDistanceMatrix(adjMatrix);
+                }
                 cout << value << endl;
                 break;
             }
-            case 3: {
+            case 2: {
                 int param;
-                long long total_distance;
+                long long total_distance = 0;
                 cin >> param;
-
-                IterativeGrowth iterativeGrowth;
-                total_distance = iterativeGrowth.iterativeGrowthAdjList(value, param);
+                if (choice == 1) {
+                    IterativeGrowth iterativeGrowth;
+                    total_distance = iterativeGrowth.iterativeGrowthAdjList(value, param);
+                }
                 cout << total_distance << endl;
                 break;
             }
-            case 4:{
+            case 3:{
                 DCN dcn;
-                vector<vector<int>> adjList = dcn.adjacencyListDCN(value);
-                value = Graph::calculateTotalDistance(adjList);
+                if (choice == 1) {
+                    vector<vector<int>> adjList = dcn.adjacencyListDCN(value);
+                    value = Graph::calculateTotalDistance(adjList);
+                }
+                else if (choice == 2){
+                    vector<vector<int>> adjMatrix = dcn.adjacencyMatrixDCN(value);
+                    value = Graph::calculateTotalDistanceMatrix(adjMatrix);
+                }
                 cout << value << endl;
                 break;
             }
-            case 6:
+            case 4:
                 string matrix;
                 cin >> matrix;
 
                 Kronecker kronecker;
-                kronecker.computeAndDisplayResults(matrix, value);
+                if (choice == 1) {
+                    vector<vector<pair<int, int>>> adjList = kronecker.adjacencyListKronecker(matrix, value);
+                    value = kronecker.calculateTotalDistance(adjList);
+                }
+                else if (choice == 2){
+                    vector<vector<int>> adjMatrix = kronecker.adjacencyMatrixKronecker(matrix, value);
+                    value = kronecker.calculateTotalGraphDistance(adjMatrix);
+                }
+                cout << value << endl;
                 break;
         }
     }
